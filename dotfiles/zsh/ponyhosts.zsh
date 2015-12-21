32a5d67c954e6776bac5ca6a6fe144ea28d1358d
AppleDash <derp@derpy.fr>
2015-12-21 03:42:47 -0500

    Add in ponyhosts

---
+++ dotfiles/zsh/ponyhosts.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# Stolen shamelessly from Moonlightning on Canternet.
+
+autoload -U colors && colors
+typeset -A ponyhosts
+ponyhosts=(
+    alicorn "%F{009}a%F{238}l%F{009}i%F{237}c%F{009}o%F{237}r%F{009}n"
+    )
+
+typeset -r ponyhosts
+hostname=${$(hostname)%%.*}
+# PROMPT="%(?.%F{green}.%F{red})[%?] %F{yellow}[%l] %F{magenta}[%/]%f"$'\n'"${ponyhosts[$hostname]:-$hostname}%f:%. %n%# "
+PROMPT="%(?.%F{green}.%F{red})[%?] %F{yellow}[%l] %F{magenta}[%/]%f"$'\n'"[%n@${ponyhosts[$hostname]:-$hostname}%f:%1~]$ "
