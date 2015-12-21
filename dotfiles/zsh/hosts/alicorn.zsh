002c7f8c1425df21812d8f5ca4f520cd6f672be4
AppleDash <derp@derpy.fr>
2015-12-21 03:31:15 -0500

    Improve zshrc

--- dotfiles/zsh/hosts/alicorn.zsh
+++ dotfiles/zsh/hosts/alicorn.zsh
@@ -1,2 +1,10 @@
 #!/bin/zsh
+# On my desktop, IntelliJ products only work under `strace` due to a buggy JVM.
+alias pycharm-strace="strace -f -e trace=open pycharm >/dev/null 2>&1 &!"
+alias intellij-strace="strace -f -e trace=open intellij-idea-ultimate-edition >/dev/null 2>&1 &!"
 
+sublime() {  
+    subl $@ > /dev/null 2>&1 &!
+}
+
+source ~/.shellfuncs/*.sh
