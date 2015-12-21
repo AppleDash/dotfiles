87a3c6da1392e93fa9c020bb2d81e840de01781f
AppleDash <derp@derpy.fr>
2015-12-21 02:18:10 -0500

    Add zsh stuff

--- init/init_zsh.zsh
+++ init/init_zsh.zsh
@@ -6,3 +6,8 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   make_link "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+rm $HOME/.zshrc
+make_link $df_base_dir/dotfiles/zshrc $HOME/.zshrc
+make_link $df_base_dir/dotfiles/bashrc $HOME/.bashrc
+
