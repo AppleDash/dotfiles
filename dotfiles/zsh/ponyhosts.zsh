03b2b9c1218d7e7d2128685606f26c8ec086a21a
AppleDash <derp@derpy.fr>
2015-12-21 06:16:41 -0500

    Add another host to ponyhosts

--- dotfiles/zsh/ponyhosts.zsh
+++ dotfiles/zsh/ponyhosts.zsh
@@ -6,6 +6,7 @@ typeset -A ponyhosts
 ponyhosts=(
     alicorn  "%F{009}a%F{238}l%F{009}i%F{237}c%F{009}o%F{237}r%F{009}n"
     duskbutt "%F{238}duskbutt"
+    ponies   "%F{039}ponies"
     )
 
 typeset -r ponyhosts
